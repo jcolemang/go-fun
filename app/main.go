12289e8b2e65ac5712c8322074c50d36a62901a6
jcolemang <9168599+jcolemang@users.noreply.github.com>
2023-07-25 22:54:11 -0500

    switch out lexer

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
     "github.com/alecthomas/participle/v2"
+	"github.com/alecthomas/participle/v2/lexer"
 	"github.com/alecthomas/kong"
 	"github.com/alecthomas/repr"
 )
@@ -39,28 +40,43 @@ type X86Program struct {
 }
 
 type X86Directive struct {
-	Name *string `"."@Ident`
-	Arg *string `@Ident`
+	Name *string `( "."@Ident`
+	Arg *string `   @Ident ) EOL`
 }
 
 type X86Instr struct {
-	Label *string  `@Ident":"`
-    Addq []*X86Arg `| "addq" @@ "," @@`
-	Movq []*X86Arg `| "movq" @@ "," @@`
-	Retq string    `| @"retq"`
+	Label *string  `( @Ident":"`
+    Addq []*X86Arg `  | "addq" @@ "," @@`
+	Movq []*X86Arg `  | "movq" @@ "," @@`
+	Retq string    `  | @"retq" ) EOL`
 }
 
 type X86Arg struct {
-    X86Int *int         `"$"@Int`
-    X86Reg *X86Register `| @@`
+    X86Int *int               `"$"@Int`
+    X86Reg *X86Register       `| @@`
+	X86Offset *int            `| @Int`
+	X86OffsetReg *X86Register `  "("@@")"`
 }
 
 type X86Register struct {
-	Name *string `"%"@("rax")`
+	Name *string `"%"@Ident`
 }
 
 func GetX86Parser() *participle.Parser[X86Program] {
-    parser := participle.MustBuild[X86Program]()
+	basicLexer := lexer.MustSimple([]lexer.SimpleRule{
+		{"Comment", `(?i)rem[^\n]*`},
+		{"String", `"(\\"|[^"])*"`},
+		{"Ident", `[a-zA-Z_]\w*`},
+		{"Int", `[-]?(\d+)`},
+		{"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
+		{"EOL", `[\n\r]+`},
+		{"whitespace", `[ \t]+`},
+	})
+
+	parser := participle.MustBuild[X86Program](
+		participle.Lexer(basicLexer),
+	)
+
     return parser
 }
 
