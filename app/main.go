f51e51c77cf9048ca01a4c31090ced71a398faa5
James Gibson <james.gibson@fastradius.com>
2023-07-25 21:37:47 -0500

    Add work

--- app/main.go
+++ app/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
+    "github.com/alecthomas/participle/v2"
 	"github.com/alecthomas/kong"
 	"github.com/alecthomas/repr"
-
-	"github.com/alecthomas/participle/v2"
-	// "github.com/alecthomas/participle/v2/lexer"
 )
 
+// Main Language
 type Program struct {
 	Expr *Expr `@@`
 }
@@ -22,25 +20,50 @@ type Expr struct {
 }
 
 type Num struct {
-	Value *float64 `@Float | @Int` 
+	Value *float64 `@Float | @Int`
 }
 
 type Var struct {
     Value *string `@Ident`
 }
 
+func GetLanguageParser() *participle.Parser[Program] {
+    parser := participle.MustBuild[Program]()
+    return parser
+}
+
+// X86 Language
+type X86Program struct {
+    X86Instrs []*X86Instrs ` "globl" "main" EOL "main:" EOL @@*`
+}
+
+type X86Instrs struct {
+    Addq []*X86Arg `"addq" @@ "," @@`
+}
+
+type X86Arg struct {
+    X86Int *int         `"$"@Int`
+    X86Reg *X86Register `| @@`
+}
+
+type X86Register struct {
+	Name *string `"%"@("rax")`
+}
+
+func GetX86Parser() *participle.Parser[X86Program] {
+    parser := participle.MustBuild[X86Program]()
+    return parser
+}
+
+
+// Code to actually run
 var cli struct {
-	EBNF  bool     `help"Dump EBNF."`
 	Files []string `arg:"" optional:"" type:"existingfile" help:"GraphQL schema files to parse."`
 }
 
 func main() {
-    parser := participle.MustBuild[Program]()
+    parser := GetX86Parser()
 	ctx := kong.Parse(&cli)
-	if cli.EBNF {
-		fmt.Println(parser.String())
-		ctx.Exit(0)
-	}
 	for _, file := range cli.Files {
 		r, err := os.Open(file)
 		ctx.FatalIfErrorf(err)
