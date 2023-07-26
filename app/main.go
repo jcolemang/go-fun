6459fc97376585bd38095a19d71e48529083c026
James Gibson <james.gibson@fastradius.com>
2023-07-26 16:41:08 -0500

    Fix language lexer

--- app/main.go
+++ app/main.go
@@ -21,7 +21,7 @@ type Expr struct {
 }
 
 type Num struct {
-	Value *float64 `@Float | @Int`
+	Value *float64 `@Int`
 }
 
 type Var struct {
@@ -29,7 +29,17 @@ type Var struct {
 }
 
 func GetLanguageParser() *participle.Parser[Program] {
-    parser := participle.MustBuild[Program]()
+	basicLexer := lexer.MustSimple([]lexer.SimpleRule{
+		{"Comment", `(?i)rem[^\n]*`},
+		{"String", `"(\\"|[^"])*"`},
+		{"Ident", `[a-zA-Z_\-+]\w*`},
+		{"Int", `[-]?(\d+)`},
+		{"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`},
+		{"whitespace", `[ \t\n\r]+`},
+	})
+    parser := participle.MustBuild[Program](
+        participle.Lexer(basicLexer),
+    )
     return parser
 }
 
@@ -87,7 +97,7 @@ var cli struct {
 }
 
 func main() {
-    parser := GetX86Parser()
+    parser := GetLanguageParser()
 	ctx := kong.Parse(&cli)
 	for _, file := range cli.Files {
 		r, err := os.Open(file)
